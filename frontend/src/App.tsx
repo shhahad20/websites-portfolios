cf8dc9591c851a16dd2cf1178c0f23d0f1d132ff
shhahad20 <Shhahad20@gmail.com>
2025-06-29 13:38:24 +0300

    ✨ feat: add Footer component with styling for improved layout and user navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HeroSection } from './pages/HeroSection';
 import About from './pages/About';
 import AuthPage from './pages/AuthPage';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+import { Link } from "react-router-dom";
+
+const Footer: React.FC = () => (
+  <footer className={styles.footer}>
+    <div className={styles.footerContent}>
+      <span>
+        &copy; 2025 SHAHAD ALTHARWA. All Rights Reserved.
+      </span>
+      <span className={styles.contactLink}>
+        <Link to="/contact">Contact Us</Link>
+      </span>
+    </div>
+  </footer>
+);
+
+export default Footer;
